8ca9961ff7c87b32bb4fd8ff7e42d459259a8e75
Akshat Sagar <akshatsagar@Akshats-MacBook-Air.local>
2025-01-06 20:41:37 +0530

    Adding new file

---
+++ module1_condiStatement.playground/Contents.swift
@@ -0,0 +1,20 @@
+let freeApp = true
+if freeApp{
+    print("You are using the free version of the app. Buy the full version of the app to get access to all of its features.")
+}
+let morningTemp = 70
+let eveningTemp = 80
+if morningTemp < eveningTemp {
+    print("\(morningTemp)")
+}
+else{
+    print("\(eveningTemp)")
+}
+let tempDegree = "Farenheit"
+if tempDegree == "Farenheit" {
+    print("the app uses farenheit")
+
+}
+else{
+    print("the app uses celcius")
+}
